fef3be91f271777db09050139ad681371339e4be
Andrej-Torsten Löhr <atl@atlnet.de>
2023-04-10 04:54:36 +0200

    fix: database and smtp passwords will set correctly in kivitendo.conf

--- docker/start.sh
+++ docker/start.sh
@@ -6,11 +6,11 @@
 [ -n "${DB_PORT}" ] && sed -i "1s/port     = .*/port     = ${DB_PORT}/" /var/www/kivitendo-erp/config/kivitendo.conf
 [ -n "${DB_DBNAME}" ] && sed -i "s/db       = .*/db       = ${DB_DBNAME}/" /var/www/kivitendo-erp/config/kivitendo.conf
 [ -n "${DB_USER}" ] && sed -i "s/user     = .*/user     = ${DB_USER}/" /var/www/kivitendo-erp/config/kivitendo.conf
-[ -n "${DB_PASSWORD}" ] && sed -i "1s/password = .*/password = ${DB_PASSWORD}/" /var/www/kivitendo-erp/config/kivitendo.conf
+[ -n "${DB_PASSWORD}" ] && sed -i "/^\[authentication\/database\]$/,/^\[/ s/^password = .*/password = ${DB_PASSWORD}/" /var/www/kivitendo-erp/config/kivitendo.conf
 [ -n "${SMTP_HOST}" ] && sed -i "s/host = .*/host = ${SMTP_HOST}/" /var/www/kivitendo-erp/config/kivitendo.conf
 [ -n "${SMTP_PORT}" ] && sed -i "s/#*port = .*/port = ${SMTP_PORT}/" /var/www/kivitendo-erp/config/kivitendo.conf
 [ -n "${SMTP_SECURITY}" ] && sed -i "s/security = .*/security = ${SMTP_SECURITY,,}/" /var/www/kivitendo-erp/config/kivitendo.conf
 [ -n "${SMTP_USER}" ] && sed -i "s/login =.*/login = ${SMTP_USER}/" /var/www/kivitendo-erp/config/kivitendo.conf
-[ -n "${SMTP_PASSWORD}" ] && sed -i "2s/password =.*/password = ${SMTP_PASSWORD}/" /var/www/kivitendo-erp/config/kivitendo.conf
+[ -n "${SMTP_PASSWORD}" ] && sed -i "/^\[mail_delivery\]$/,/^\[/ s/^password =.*/password = ${SMTP_PASSWORD}/" /var/www/kivitendo-erp/config/kivitendo.conf
 
 exec supervisord -n
